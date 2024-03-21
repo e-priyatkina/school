c159ac18a445ef04b0a4f8c7e8cee98763432312
e-priyatkina <priyatkinaekaterina@gmail.com>
2024-03-21 14:19:32 +0300

    school8.0

--- src/main/java/ru/hogwarts/school/scripts422.sql
+++ src/main/java/ru/hogwarts/school/scripts422.sql
@@ -1,18 +1,18 @@
+CREATE TABLE cars (
+    car_id SERIAL PRIMARY KEY,
+    brand TEXT,
+    model TEXT,
+    price INTEGER CHECK (price > 0)
+);
+
 CREATE TABLE person (
     person_id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     age INTEGER CHECK (age > 0),
     car TEXT NOT NULL,
     car_license BOOLEAN,
-    car_id TEXT REFERENCES cars (person_id)
+    car_id SERIAL REFERENCES cars (person_id)
 );
-
-CREATE TABLE cars (
-    car_id SERIAL PRIMARY KEY,
-    brand TEXT,
-    model TEXT,
-    price INTEGER CHECK (price > 0)
-)
 
 SELECT person.name, person.age, person.car, person.car_license, cars.brand, cars.model, cars.price
 FROM person
